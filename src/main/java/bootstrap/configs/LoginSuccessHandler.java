302f80fa6515e75bde85f9c81e38affeb92fee01
Евгения <jane_79@list.ru>
2022-09-05 14:09:50 +0300

    configs are amended

--- src/main/java/bootstrap/configs/LoginSuccessHandler.java
+++ src/main/java/bootstrap/configs/LoginSuccessHandler.java
@@ -2,7 +2,9 @@ package bootstrap.configs;
 
 import bootstrap.model.Role;
 import bootstrap.model.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -10,20 +12,21 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Set;
 
+@Slf4j
 @Component
 public class LoginSuccessHandler implements AuthenticationSuccessHandler {
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,
                                         HttpServletResponse response,
-                                        Authentication authentication) throws IOException, ServletException {
-
-        User user = (User) authentication.getPrincipal();
-
-        if (user.getRoles().contains(new Role("ADMIN"))) {
+                                        Authentication authentication) throws IOException {
+        Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
+        log.info("roles: {}", roles);
+        if (roles.contains("ROLE_ADMIN")) {
             response.sendRedirect("/");
-        } else if (user.getRoles().contains(new Role("USER"))) {
+        } else if (roles.contains("ROLE_USER")) {
             response.sendRedirect("/");
         } else {
             response.sendRedirect("/login");
